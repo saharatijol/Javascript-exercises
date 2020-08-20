6a00dcf14f6ca17eecb5b55f51c5a1626a32910a
Sara T <sahara.tijol@gmail.com>
2020-08-19 23:55:34 -0500

    whos paying walkthrough

--- Angela Yu's course/whosPaying.js
+++ Angela Yu's course/whosPaying.js
@@ -11,9 +11,20 @@ Angela is buying lunch today!
 Hint:
 -You might need to think about Array.length
 */
-names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
 
+
+names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+// My solution //
 function whosPaying(names) {
   var randomName = names[Math.floor(Math.random() * names.length)];
   return (randomName + " is going to buy lunch today!");
+}
+
+//Angela's Solution//
+function whosPaying(names) {
+  var numberOfPeople = names.length;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  var randomPerson = names[randomPersonPosition];
+
+  return randomPerson + " is going to buy lunch today!";
 }
